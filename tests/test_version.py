5a5bdb9f5eff34bc05a18d09de16b4533e22b7b1
AFis-245 <annika.fischer@e-box3.de>
2025-06-03 09:42:07 -0500

    update test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -7,5 +7,6 @@ def test_version():
     with open("./pyproject.toml", "r") as tomlfile:
         contents = tomlfile.read()
         parsed_contents = tomlkit.parse(contents)
-    loaded_version = parsed_contents["tool"]["poetry"]["version"]
+    #loaded_version = parsed_contents["tool"]["poetry"]["version"]
+    loaded_version = parsed_contents["project"]["version"]
     assert flirextractor.__version__ == loaded_version
